cf984fdeb4f4f5969954fe39341f386dabc88ad4
RonaldoDairo <programaciondairo@gmail.com>
2023-03-25 13:18:06 +0000

    agg project realizo de relacion de base de datos

--- src/models.py
+++ src/models.py
@@ -8,26 +8,64 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class User(Base):
+    __tablename__ = 'user'
+    ID= Column(Integer, primary_key=True )
+    user_name = Column(Integer, unique=True)
+    password = Column( String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    email = Column(String(250) ,unique=True)
 
-    def to_dict(self):
-        return {}
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    ID= Column(Integer, primary_key=True)
+    descripcion = Column(String(250))
+    historia = Column(String(250))
+    tipos = Column(String(250))
+
+class People(Base):
+    __tablename__ = 'people'
+    ID= Column(Integer, primary_key=True )
+    descripcion = Column(String(250))
+    historia = Column(String(250))
+    trayectoria = Column(String(250))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    ID= Column(Integer, primary_key=True)
+    descripcion = Column(String(250))
+    historia = Column(String(250))
+    modelo= Column(String(250))
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    ID= Column(Integer, primary_key=True )
+    people_id = Column(Integer, ForeignKey('people.ID') )
+    people = relationship(People)
+    planet_id = Column(Integer , ForeignKey('planets.ID'))
+    planet = relationship(Planets)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.ID'))
+    vehicle = relationship(Vehicles)
+    user_id = Column(Integer, ForeignKey('user.ID') )
+    user_id_favorite = relationship(User)
+    
+
+    
+
+    
+
+
+
+
+
+
+    
+
+
+ 
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
